86bea043c54cf3caeceff069fa6057f5441cd1af
Kassandra Flores <kflores418@gmail.com>
2026-04-05 21:42:20 -0700

    HW DONE :)

--- String_Parser.cpp
+++ String_Parser.cpp
@@ -4,10 +4,33 @@
 #include <sstream> // For parsing lines
 using namespace std;
 
-void parseCSV(const string&);
+void parseCSV(const string& filename);
 
-int main() {
+int main() 
+{   
    parseCSV("students.csv");
-
    return 0;
+}
+
+
+void parseCSV(const string& filename)
+{
+   ifstream inStream(filename);
+   string word;
+
+   while (getline(inStream, word, ' '))
+   {
+      for (int i = 0; i < word.length(); i++)
+      {
+         cout << word[i];
+            if(word[i] == ':')
+            {
+               cout << ' ';
+            }
+      }
+
+      cout << endl;
+   }
+
+   inStream.close();
 }
